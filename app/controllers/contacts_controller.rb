13d813fd493946d69e376892b98e9e3789b42fe1
hongtaolee <hongtaolee@gmail.com>
2012-01-06 18:14:38 +0800

    update nginx

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -69,7 +69,7 @@ class ContactsController < ApplicationController
   def new
     @contact  = Contact.new(:user => @current_user, :access => Setting.default_access)
     @account  = Account.new(:user => @current_user)
-    @accounts = get_accounts
+    @accounts = Account.all
     if params[:related]
       model, id = params[:related].split("_")
       instance_variable_set("@#{model}", model.classify.constantize.my.find(id))
